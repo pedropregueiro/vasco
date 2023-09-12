cf3f7d7f006b08da5fa670cf7426d880b26c107a
prego <pedropregueiro@gmail.com>
2023-09-12 22:10:12 +0200

    make a home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,65 @@
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      {/* <main className={styles.main}> */}
-      <p>Home</p>
+    <div
+      style={{
+        width: "100%",
+        minHeight: "80vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        <h2>Vasco</h2>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "10rem 10rem",
+            columnGap: "2rem",
+          }}
+        >
+          <Link
+            href="/fid/3"
+            style={{
+              border: "1px solid black",
+              borderRadius: "4px",
+              textAlign: "center",
+            }}
+          >
+            <p>@dwr.eth</p>
+            <p
+              style={{
+                color: "#222",
+                fontSize: "0.8rem",
+                textTransform: "lowercase",
+              }}
+            >
+              /fid/:fid
+            </p>
+          </Link>
+          <Link
+            href="/cast/0x4f909cacf0d364ff8c495bf860092d5cb976a2b2"
+            style={{
+              border: "1px solid black",
+              borderRadius: "4px",
+              textAlign: "center",
+            }}
+          >
+            <p>Vitalik's cast</p>
+            <p
+              style={{
+                color: "#222",
+                fontSize: "0.8rem",
+                textTransform: "lowercase",
+              }}
+            >
+              /cast/:hash
+            </p>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
