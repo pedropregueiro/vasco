8fa04ec2d0c17d30a89a1d44aa0e8682ef596d39
prego <pedropregueiro@gmail.com>
2023-09-25 22:17:36 +0200

    better homepage

--- app/fid/[id]/signers/page.js
+++ app/fid/[id]/signers/page.js
@@ -5,6 +5,8 @@ import {
 } from "@/src/hooks/hub";
 
 import Signer from "@/src/components/signer";
+import { Suspense } from "react";
+import UserCard from "@/src/components/user-card";
 
 export const revalidate = 0;
 
@@ -24,17 +26,36 @@ export default async function Signers({ params }) {
         }}
       >
         <h2>Signers</h2>
+        <Suspense fallback={<div>Loading...</div>}>
+          <UserCard fid={fid} />
+        </Suspense>
       </div>
 
       <div className="three-column-grid">
         {signers.map((signer) => (
-          <Signer
+          <Suspense
             key={signer?.signerEventBody?.key}
-            fid={fid}
-            signer={signer}
-            castsCount={casts.length}
-            reactionsCount={reactions.length}
-          />
+            fallback={
+              <div
+                style={{
+                  marginTop: "2rem",
+                  minHeight: "10rem",
+                  border: "1px solid #ddd",
+                  borderRadius: "4px",
+                  padding: "1rem",
+                }}
+              >
+                Loading...
+              </div>
+            }
+          >
+            <Signer
+              fid={fid}
+              signer={signer}
+              castsCount={casts.length}
+              reactionsCount={reactions.length}
+            />
+          </Suspense>
         ))}
       </div>
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,32 @@
 import { Footer } from "@/src/components/footer";
 import Link from "next/link";
 
+const HomeCard = ({ title, subtitle, link }) => {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        borderRadius: "4px",
+        textAlign: "center",
+        padding: "1.5rem",
+      }}
+    >
+      <Link href={link}>
+        <p>{title}</p>
+        <p
+          style={{
+            color: "#222",
+            fontSize: "0.8rem",
+            textTransform: "lowercase",
+          }}
+        >
+          {subtitle}
+        </p>
+      </Link>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -18,67 +44,40 @@ export default function Home() {
           <div
             style={{
               display: "grid",
-              gridTemplateColumns: "10rem 10rem",
+              gridTemplateColumns: "15rem 15rem 15rem",
               gap: "2rem",
             }}
           >
-            <Link
-              href="/fid/3"
-              style={{
-                border: "1px solid black",
-                borderRadius: "4px",
-                textAlign: "center",
-              }}
-            >
-              <p>@dwr.eth</p>
-              <p
-                style={{
-                  color: "#222",
-                  fontSize: "0.8rem",
-                  textTransform: "lowercase",
-                }}
-              >
-                /fid/:fid
-              </p>
-            </Link>
-            <Link
-              href="/cast/0x4f909cacf0d364ff8c495bf860092d5cb976a2b2"
-              style={{
-                border: "1px solid black",
-                borderRadius: "4px",
-                textAlign: "center",
-              }}
-            >
-              <p>Random cast</p>
-              <p
-                style={{
-                  color: "#222",
-                  fontSize: "0.8rem",
-                  textTransform: "lowercase",
-                }}
-              >
-                /cast/:hash
-              </p>
-            </Link>
-            <Link
-              href="/varunsrin.eth"
-              style={{
-                border: "1px solid black",
-                borderRadius: "4px",
-                textAlign: "center",
-              }}
-            >
-              <p>varunsrin.eth</p>
-              <p
-                style={{
-                  color: "#222",
-                  fontSize: "0.8rem",
-                  textTransform: "lowercase",
-                }}
-              >
-                /:username
-              </p>
-            </Link>
+            <HomeCard
+              title="@varunsrin.eth"
+              subtitle="/fid/:fid"
+              link="/fid/2"
+            />
+            <HomeCard title="dwr.eth" subtitle="/:username" link="/dwr.eth" />
+
+            <HomeCard
+              title="Cast"
+              subtitle="/cast/:hash"
+              link="/cast/0x4f909cacf0d364ff8c495bf860092d5cb976a2b2"
+            />
+
+            <HomeCard
+              title="Signers"
+              subtitle="/fid/:fid/signers"
+              link="/fid/2/signers"
+            />
+
+            <HomeCard
+              title="Signer"
+              subtitle="/fid/:fid/signer/:signer"
+              link="/fid/2/signer/0x78ff9a768cf1fbb13caaf5159a9623dd2499b01592a0ee672eca647b6d62558c"
+            />
+
+            <HomeCard
+              title="GetInfo (RPC)"
+              subtitle="/rpc/:method"
+              link="/rpc/getInfo"
+            />
           </div>
         </div>
       </div>
